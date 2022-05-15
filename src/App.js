d233c035b211e579ff0780c957065d3aad46f809
Andrew Mwendo <andrew.mwendo@technicolor.com>
2022-05-15 12:59:57 +0200

    make it better

--- src/App.js
+++ src/App.js
@@ -23,63 +23,74 @@ export default function App() {
           y1={divCoordinates.one !== undefined ? divCoordinates.one.y : 0}
           x2={divCoordinates.eight !== undefined ? divCoordinates.eight.x : 0}
           y2={divCoordinates.eight !== undefined ? divCoordinates.eight.y : 0}
-          stroke={"yellow"}
+          id='line1'
+          stroke='yellow'
         />
         <line
-          x1={divCoordinates.eight !== undefined ? divCoordinates.eight.x : 0}
-          y1={divCoordinates.eight !== undefined ? divCoordinates.eight.y : 0}
-          x2={divCoordinates.three !== undefined ? divCoordinates.three.x : 0}
-          y2={divCoordinates.three !== undefined ? divCoordinates.three.y : 0}
-          stroke="yellow"
+          
+          x1={divCoordinates.three !== undefined ? divCoordinates.three.x : 0}
+          y1={divCoordinates.three !== undefined ? divCoordinates.three.y : 0}
+          x2={divCoordinates.eight !== undefined ? divCoordinates.eight.x : 0}
+          y2={divCoordinates.eight !== undefined ? divCoordinates.eight.y : 0}
+          id='line2'
+          stroke='yellow'
         />
         <line
-          x1={divCoordinates.eight !== undefined ? divCoordinates.eight.x : 0}
-          y1={divCoordinates.eight !== undefined ? divCoordinates.eight.y : 0}
-          x2={divCoordinates.five !== undefined ? divCoordinates.five.x : 0}
-          y2={divCoordinates.five !== undefined ? divCoordinates.five.y : 0}
-          stroke="yellow"
+          
+          x1={divCoordinates.five !== undefined ? divCoordinates.five.x : 0}
+          y1={divCoordinates.five !== undefined ? divCoordinates.five.y : 0}
+          x2={divCoordinates.eight !== undefined ? divCoordinates.eight.x : 0}
+          y2={divCoordinates.eight !== undefined ? divCoordinates.eight.y : 0}
+          id='line3'
+          stroke='yellow'
         />
         <line
           x1={divCoordinates.two !== undefined ? divCoordinates.two.x : 0}
           y1={divCoordinates.two !== undefined ? divCoordinates.two.y : 0}
           x2={divCoordinates.four !== undefined ? divCoordinates.four.x : 0}
           y2={divCoordinates.four !== undefined ? divCoordinates.four.y : 0}
-          stroke="yellow"
+          id='line4'
+          stroke='yellow'
         />
         <line
           x1={divCoordinates.three !== undefined ? divCoordinates.three.x : 0}
           y1={divCoordinates.three !== undefined ? divCoordinates.three.y : 0}
           x2={divCoordinates.five !== undefined ? divCoordinates.five.x : 0}
           y2={divCoordinates.five !== undefined ? divCoordinates.five.y : 0}
-          stroke="yellow"
+          id='line5'
+          stroke='yellow'
         />
         <line
           x1={divCoordinates.four !== undefined ? divCoordinates.four.x : 0}
           y1={divCoordinates.four !== undefined ? divCoordinates.four.y : 0}
           x2={divCoordinates.five !== undefined ? divCoordinates.five.x : 0}
           y2={divCoordinates.five !== undefined ? divCoordinates.five.y : 0}
-          stroke="yellow"
+          id='line6'
+          stroke='yellow'
         />
         <line
           x1={divCoordinates.six !== undefined ? divCoordinates.six.x : 0}
           y1={divCoordinates.six !== undefined ? divCoordinates.six.y : 0}
           x2={divCoordinates.five !== undefined ? divCoordinates.five.x : 0}
           y2={divCoordinates.five !== undefined ? divCoordinates.five.y : 0}
-          stroke="yellow"
+          id='line7'
+          stroke='yellow'
         />
         <line
           x1={divCoordinates.seven !== undefined ? divCoordinates.seven.x : 0}
           y1={divCoordinates.seven !== undefined ? divCoordinates.seven.y : 0}
           x2={divCoordinates.eight !== undefined ? divCoordinates.eight.x : 0}
           y2={divCoordinates.eight !== undefined ? divCoordinates.eight.y : 0}
-          stroke="yellow"
+          id='line8'
+          stroke='yellow'
         />
         <line
           x1={divCoordinates.seven !== undefined ? divCoordinates.seven.x : 0}
           y1={divCoordinates.seven !== undefined ? divCoordinates.seven.y : 0}
           x2={divCoordinates.nine !== undefined ? divCoordinates.nine.x : 0}
           y2={divCoordinates.nine !== undefined ? divCoordinates.nine.y : 0}
-          stroke="yellow"
+          id='line9'
+          stroke='yellow'
         />
       </svg>
     </div>
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -4,10 +4,12 @@ const spiderCombinations = [['one,eight'], ['eight', 'three'], ['eight', 'five']
 
 export default function UseFetch() {
   const [divCoordinates, setDivCoordinates] = useState({});
-  const [hasIntersected,setHasIntersected] = useState()
+  const [hasIntersected, setHasIntersected] = useState()
+  const [isDragging,setIsDragging] = useState(false)
 
   const dragProps = useRef();
-
+  
+  
   const initialiseDrag = (event) => {
     const id = event.target.id;
     const current = document.getElementById(id);
@@ -20,7 +22,67 @@ export default function UseFetch() {
         },
       };
       setDivCoordinates(obj);
-
+      setIsDragging(!isDragging)
+      const lineSegmentsIntersect = (x1, y1, x2, y2, x3, y3, x4, y4,item) => {
+     
+        const a_dx = x2 - x1;
+       const a_dy = y2 - y1;
+       const b_dx = x4 - x3;
+       const b_dy = y4 - y3;
+       const s = (-a_dy * (x1 - x3) + a_dx * (y1 - y3)) / (-b_dx * a_dy + a_dx * b_dy);
+        const t = (+b_dx * (y1 - y3) - b_dy * (x1 - x3)) / (-b_dx * a_dy + a_dx * b_dy);
+        const intersected = (() => {
+          if(s >= 0 &&
+            s <= 1 &&
+            t >= 0 &&
+            t <= 1) return true
+          return false
+        })()
+        
+        if (
+          intersected &&
+            x1 !== x3 &&
+            x1 !== x4 &&
+            x2 !== x3 &&
+            x2 !== x4 && 
+            y1 !== y3 &&
+            y1 !== y4 &&
+            y2 !== y3 &&
+            y2 !== y4) {
+            item[0].setAttribute("stroke", "red")
+          item[1].setAttribute("stroke", "red")
+        } else {
+          item[0].setAttribute("stroke", "yellow")
+          item[1].setAttribute("stroke", "yellow")
+        }
+      }
+      let line1 = document.getElementById("line1");
+    let line2 = document.getElementById("line2");
+    let line3 = document.getElementById("line3");
+    let line4 = document.getElementById("line4");
+    let line5 = document.getElementById("line5");
+    let line6 = document.getElementById("line6");
+    let line7 = document.getElementById("line7");
+    let line8 = document.getElementById("line8");
+    let line9 = document.getElementById("line9");
+    let newSpidersArray = [line1, line2, line3, line4, line5, line6, line7, line8, line9];
+      const lineIntersectionCombination = newSpidersArray.flatMap(
+        (v, i) => newSpidersArray.slice(i+1).map(w => [v , w] )
+      );
+  
+      lineIntersectionCombination.forEach((item) => {
+        let x1 = item[0].x1.baseVal.value-10
+        let y1 = item[0].y1.animVal.value-10
+        let x2 = item[0].x2.baseVal.value-10
+        let y2 = item[0].y2.animVal.value-10
+        let x3 = item[1].x1.baseVal.value-10
+        let y3 = item[1].y1.animVal.value-10
+        let x4 = item[1].x2.baseVal.value-10
+        let y4 = item[1].y2.animVal.value-10
+        lineSegmentsIntersect(x1, y1, x2, y2, x3, y3, x4, y4,item)
+      })
+      
+      
       current.style.transform = `translate(${
         dragProps.current.dragStartLeft + clientX - dragProps.current.dragStartX
       }px, ${
@@ -49,23 +111,69 @@ export default function UseFetch() {
   };
 
   useEffect(() => {
-    const newSpidersArray = [...spiderCombinations]
-    const lineIntersectionCombination = newSpidersArray.flatMap(
-      (v, i) => newSpidersArray.slice(i+1).map( w => v + ',' + w )
-    );
-  
-    const lineSegmentsIntersect = (x1, y1, x2, y2, x3, y3, x4, y4) => {
+    
+   if(Object.getOwnPropertyNames(divCoordinates).length === 0 ) return
+   let line1 = document.getElementById("line1");
+   let line2 = document.getElementById("line2");
+   let line3 = document.getElementById("line3");
+   let line4 = document.getElementById("line4");
+   let line5 = document.getElementById("line5");
+   let line6 = document.getElementById("line6");
+   let line7 = document.getElementById("line7");
+   let line8 = document.getElementById("line8");
+   let line9 = document.getElementById("line9");
+   let newSpidersArray = [line1, line2, line3, line4, line5, line6, line7, line8, line9];
+    const lineSegmentsIntersect = (x1, y1, x2, y2, x3, y3, x4, y4,item) => {
      
       const a_dx = x2 - x1;
-      const a_dy = y2 - y1;
-      const b_dx = x4 - x3;
-      const b_dy = y4 - y3;
-      const s = (-a_dy * (x1 - x3) + a_dx * (y1 - y3)) / (-b_dx * a_dy + a_dx * b_dy);
+     const a_dy = y2 - y1;
+     const b_dx = x4 - x3;
+     const b_dy = y4 - y3;
+     const s = (-a_dy * (x1 - x3) + a_dx * (y1 - y3)) / (-b_dx * a_dy + a_dx * b_dy);
       const t = (+b_dx * (y1 - y3) - b_dy * (x1 - x3)) / (-b_dx * a_dy + a_dx * b_dy);
-      setHasIntersected(s >= 0 && s <= 1 && t >= 0 && t <= 1);
-    } 
-    lineSegmentsIntersect()
-  },[hasIntersected])
+      const intersected = (() => {
+        if(s >= 0 &&
+          s <= 1 &&
+          t >= 0 &&
+          t <= 1) return true
+        return false
+      })()
+      if (
+        intersected &&
+          x1 !== x3 &&
+          x1 !== x4 &&
+          x2 !== x3 &&
+          x2 !== x4 && 
+          y1 !== y3 &&
+          y1 !== y4 &&
+          y2 !== y3 &&
+          y2 !== y4) {
+          item[0].setAttribute("stroke", "red")
+        item[1].setAttribute("stroke", "red")
+      } 
+  }
+
+    const lineIntersectionCombination = newSpidersArray.flatMap(
+      (v, i) => newSpidersArray.slice(i+1).map(w => [v , w] )
+    );
+
+    lineIntersectionCombination.forEach((item,index) => {
+      
+
+       
+  let x1 = item[0].x1.baseVal.value-10
+  let y1 = item[0].y1.animVal.value-10
+  let x2 = item[0].x2.baseVal.value-10
+  let y2 = item[0].y2.animVal.value-10
+  let x3 = item[1].x1.baseVal.value-10
+  let y3 = item[1].y1.animVal.value-10
+  let x4 = item[1].x2.baseVal.value-10
+  let y4 = item[1].y2.animVal.value-10
+      lineSegmentsIntersect(x1, y1, x2, y2, x3, y3, x4, y4,item)
+    })
+  
+    
+  },[hasIntersected,divCoordinates])
 
   useEffect(() => {
     let spider1 = document.getElementById("one");
